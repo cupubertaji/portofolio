d64a0cf6f62ca4318ff085d48542e36012f7643a
cupubertaji <nbprayuga@gmail.com>
2013-10-27 19:31:55 +0700

    first inizialitation project

---
+++ portofolio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portofolio::Application.config.secret_token = 'e0ae0e89a7bfaf310c483c63abd68259cc7afc1329c7e870b389fde96a6ff4765ade0225e1584e820159d16f20e06f4cf04d1a92256388af3cf25effdaebba1a'
